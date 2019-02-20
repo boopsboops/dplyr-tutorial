e069d837db042d23505f29a26c033513231a5370
rupert@it031740 <rupertcollins@gmail.com>
2019-02-20 23:26:45 +0000

    cleaned code, added month plot

--- movies.R
+++ movies.R
@@ -1,14 +1,12 @@
-library(ggplot2movies)
 library(tidyverse)
+library(lubridate)
 library(magrittr)
 library(parallel)
 library(omdbapi)#https://github.com/hrbrmstr/omdbapi
 #sudo R; devtools::install_github("hrbrmstr/omdbapi")
 
-# data(movies)
-# glimpse(movies)
-
 # load up a 5000+ movie table from a researcher
+# https://github.com/sundeepblue/movie_rating_prediction
 mm.df <- read_csv("https://github.com/sundeepblue/movie_rating_prediction/raw/master/movie_metadata.csv")
 # take a look
 glimpse(mm.df)
@@ -27,7 +25,7 @@ daily.four <- mclapply(imdb.ids[3000:3999],find_by_id,api_key="2c95f6b7", includ
 daily.five <- mclapply(imdb.ids[4000:4999],find_by_id,api_key="2c95f6b7", include_tomatoes=TRUE)
 daily.six <- mclapply(imdb.ids[5000:5043],find_by_id,api_key="2c95f6b7", include_tomatoes=TRUE)
 # create daily copy
-daily.df <- daily.one
+daily.df <- daily.two
 
 # join all the data frames
 # first extract the rotten tomato ratings from the ratings lists using Map
@@ -41,64 +39,126 @@ daily.df %>%
     write_csv("dailyX.csv")
     # remember to change file name by hand
 
-# read back in 
-omdb.df <- read_csv(file="daily1.csv", na=c("","NA","N/A"))
-
-omdb.df %<>% 
-    rename(tconst=imdbID) %>% 
-    mutate(tomatoMeter=as.numeric(str_replace_all(tomatoMeter,"%",""))) %>% 
-    select(tconst,tomatoMeter)
+    
+### analyse the final datasets 
 
 
-# join and clean
-mm.df %<>% 
-    left_join(omdb.df,by="tconst") %>% 
-    select(tconst,movie_title,title_year,director_name,duration,genres,actor_1_name,actor_1_facebook_likes,content_rating,budget,gross,imdb_score,tomatoMeter) %>%
-    mutate(genreSingle=str_replace_all(genres,"\\|.+",""))
+# read in the list of tomato files
+movies.data.list <- lapply(list.files(pattern=".csv"),read_csv,na=c("","NA","N/A"))
+
+# join together
+movies.data <- bind_rows(movies.data.list)
+
+# take a look
+movies.data %>% glimpse()
+
+# edit and remove junk columns
+movies.data %<>% 
+    mutate(
+    tomatoMeter=as.numeric(str_replace_all(tomatoMeter,"%","")), 
+    Released=ymd(Released), 
+    BoxOffice=as.numeric(str_replace_all(BoxOffice,"\\$|,","")),
+    Runtime=as.numeric(str_replace_all(Runtime," min","")),
+    #Year=parse_date_time(Year, "%Y")),
+    month=month(Released,label=TRUE)) %>%
+    select(-Type,-tomatoImage,-tomatoRating,-tomatoReviews,-tomatoFresh,-tomatoRotten,-tomatoConsensus,-tomatoUserMeter,-tomatoUserRating,-tomatoUserReviews,-tomatoURL,-DVD,-Website,-Response,-Season,-Episode,-seriesID,-totalSeasons,-ratingSite)
+
+# take a look
+movies.data %>% glimpse()
+
+
+### read in the budget data
+budget.data <- read_tsv("movie-budgets.tsv")
+
+# get IMDB id and remove other cols
+budget.data %<>% mutate(imdbID=str_split_fixed(url,"/",6)[,5]) %>%
+    select(imdbID,budget,gross)
+
+# look at the data
+budget.data %>% glimpse()
+
+# join with 
+movies.data %<>% left_join(budget.data,by="imdbID")
+
+# add any missing BoxOffice data
+movies.data %>% filter(is.na(BoxOffice))
+movies.data %<>% mutate(BoxOffice=if_else(is.na(BoxOffice),gross,BoxOffice))
+
+# make a quick summary of some stats
+movies.data %>% 
+    summarise(
+    nmovies=length(unique(imdbID)), 
+    maxDur=max(Runtime,na.rm=TRUE), 
+    minDur=min(Runtime,na.rm=TRUE), 
+    minBudget=min(budget,na.rm=TRUE), 
+    maxBudget=max(budget,na.rm=TRUE),
+    minYear=min(Year,na.rm=TRUE), 
+    maxYear=max(Year,na.rm=TRUE))
+
+
+# plot movies per year
+movies.data %>% ggplot(aes(Year)) + geom_histogram(fill="tomato") + theme_bw()
+
+# get all the unique rows
+movies.data %<>% 
+    distinct() %>% 
+    filter(between(Runtime,60,240), between(budget,500000,500000000), between(Year,1977,max(Year,na.rm=TRUE)))
+    # filter(Runtime >= 60 & Runtime <= 240 & budget >= 500000 & budget <= 500000000 & Year >= 1977)
+    #filter(!is.na(imdbRating) & !is.na(budget) & !is.na(gross))
 
 # plot tomato against imdb
-mm.df %>% filter(!is.na(tomatoMeter) & !is.na(imdb_score) & !is.na(budget) & !is.na(gross)) %>% 
-    filter(budget >= 500000 & budget <= 500000000) %>%
-    ggplot(aes(x=tomatoMeter,y=imdb_score,size=gross),na.rm=TRUE) + 
-    geom_point(alpha=0.75,shape=16,colour="tomato") + #
-    #scale_color_gradient(low="yellow", high="tomato") + 
+movies.data %>% filter(!is.na(tomatoMeter) & !is.na(imdbRating) & !is.na(budget) & !is.na(BoxOffice)) %>% 
+    ggplot(aes(x=tomatoMeter,y=imdbRating,size=budget)) + # CHANGE "BoxOffice" to "budget"
+    geom_point(alpha=0.3,shape=16,colour="tomato") +
+    #geom_point(alpha=0.5,shape=16) +
+    #scale_color_gradient(low="yellow", high="red") + 
     theme_bw()
 
-
 # gather by genre
-hh <- mm.df %>% 
-    separate(genres, into=c("genre1","genre2","genre3","genre4","genre5","genre6","genre7","genre8","genre9"), sep="\\|") %>% 
-    gather(key="ngenre",value="genre",genre1,genre2,genre3,genre4,genre5,genre6,genre7,genre8,genre9, na.rm=TRUE) %>% 
-    group_by(genre) %>% 
-    mutate(ngenre=length(unique(tconst))) %>% 
-    ungroup() 
+movies.data.genre <- movies.data %>% 
+    separate(Genre, into=c("genre1","genre2","genre3","genre4","genre5","genre6","genre7","genre8"), sep=", ") %>% 
+    gather(key="genreN",value="Genre",genre1,genre2,genre3,genre4,genre5,genre6,genre7,genre8, na.rm=TRUE) %>% 
+    group_by(Genre) %>% 
+    mutate(perGenre=length(unique(imdbID))) %>% 
+    ungroup() %>% 
+    select(-genreN) %>% 
+    mutate(Genre=as_factor(Genre))
 
 # filter outliers and plot
-hh  %>% 
-    #filter(!is.na(budget), !is.na(gross)) %>% 
-    filter(ngenre > 100 & budget >= 500000 & budget <= 500000000) %>% 
-    ggplot(aes(x=budget,y=imdb_score),na.rm=TRUE) + 
-    #ggplot(aes(x=budget,y=gross)) + 
+movies.data.genre  %>% 
+    filter(!is.na(budget) & !is.na(BoxOffice) & !is.na(imdbRating) & !is.na(tomatoMeter)) %>% 
+    filter(perGenre > 10) %>% 
+    ggplot(aes(x=budget,y=BoxOffice)) + # change y= to "BoxOffice", "imdbRating", or "tomatoMeter"
     geom_point(alpha=0.2,shape=18) + 
     scale_x_log10() + 
     scale_y_log10() + 
     geom_smooth(method="lm") + 
-    facet_wrap(~genre, scales="free") + 
+    facet_wrap(~Genre, scales="free") + 
     theme_bw()
 
+
 # look for the best and worst directors
-hh %>% filter(!is.na(director_name)) %>% 
-    group_by(director_name) %>% 
-    summarise(mean=mean(imdb_score),sd=sd(imdb_score),nmovies=length(unique(tconst))) %>%
-    filter(nmovies >= 5) %>%
-    #arrange(desc(mean)) %>% 
-    arrange(desc(sd)) %>% 
+movies.data %>% 
+    filter(!is.na(Director) & !is.na(imdbRating)) %>% 
+    mutate(Director=str_replace_all(Director,", .+","")) %>% 
+    group_by(Director) %>% 
+    summarise(meanIMDB=mean(imdbRating),sd=sd(imdbRating),nMovies=length(unique(imdbID))) %>%
+    filter(nMovies >= 5) %>%
+    arrange(desc(meanIMDB)) %>% # best
+    #arrange(desc(sd)) %>% # worst
     print(n=20)
 
-    
-    
-    
-    
+
+# takings by month
+movies.data %>% 
+    filter(!is.na(BoxOffice) & !is.na(month)) %>% 
+    group_by(month) %>% 
+    summarise(meanBox=mean(BoxOffice),sd=sd(BoxOffice),n=length(unique(imdbID)),CI=qnorm(0.975)*sd/sqrt(n)) %>% 
+    ggplot(aes(x=month,y=meanBox,group=1)) + 
+    geom_ribbon(aes(ymax=meanBox+CI,ymin=meanBox-CI,group=1),fill="tomato",alpha=0.3) + 
+    geom_line() +
+    theme_bw()
+
 
 
 # https://minimaxir.com/2018/07/imdb-data-analysis/
